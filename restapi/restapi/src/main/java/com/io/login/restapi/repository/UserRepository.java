ffdcab36da28f59d0e35e297aa1e60148e24f2e5
tayyib-shaik <62417579+tayyib-shaik0@users.noreply.github.com>
2021-03-01 11:52:36 +0530

    Delete restapi/restapi directory

--- restapi/restapi/src/main/java/com/io/login/restapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.io.login.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.io.login.restapi.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
